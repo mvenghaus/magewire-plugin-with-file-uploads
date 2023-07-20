b40341db08178ad7bd4c93733bd1ab34b0a3224e
Marcus <marcus.venghaus@inklammern.de>
2023-07-20 13:34:36 +0200

    fixed temp meta generation

--- src/Helper/TempFile.php
+++ src/Helper/TempFile.php
@@ -9,18 +9,15 @@ class TempFile
     public static function generateHashNameWithOriginalNameEmbedded(string $file): string
     {
         $hash = uniqid();
-        $meta = str_replace('/', '_', '-meta' . base64_encode($file) . '-');
-        $extension = '.' . pathinfo($file, PATHINFO_EXTENSION);
 
-        return $hash . $meta . $extension;
+        return $hash . '--meta--' . base64_encode($file);
     }
 
     public static function extractOriginalNameFromFilePath(string $file): string
     {
-        return base64_decode(
-            array_first(
-                explode('-', array_last(explode('-meta', str_replace('_', '/', $file))))
-            )
-        );
+        $data = explode('--meta--', $file);
+
+        return base64_decode(end($data));
     }
 }
+
